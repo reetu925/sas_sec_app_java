821ea7c1f2fa076a41be0a56bb0e3d8c43948b1c
reetu925 <mnreetu@gmail.com>
2020-05-13 14:08:45 +0530

    code

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,8 @@ package com.example.demo;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -14,15 +14,24 @@ public class DemoApplication {
 	
 	public static void main(String[] args) throws Exception{
 		SpringApplication.run(DemoApplication.class, args);
-		  Class.forName("com.mysql.cj.jdbc.Driver"); 
+		 
 
 	      // Step 2: Establish the connection to the database 
+	      //String url = "jdbc:mysql://10.1.101.52:3306/sec_schema"; 
 	      //String url = "jdbc:mysql://localhost:3306/sec_schema"; 
-	      String url = "jdbc:mysql://10.1.101.52:3306/sec_schema"; 
+	      String url =  "jdbc:mysql://localhost:3306/sec_schema?user=mysql&password=root";
 	      System.out.println("This statement Before connection");
-	      Connection conn = DriverManager.getConnection(url,"mysql","root"); 
-		  //Connection conn = DriverManager.getConnection(url,"root","mysql");  
+	      try {
+	    	  Class.forName("com.mysql.cj.jdbc.Driver").newInstance(); 
+	      Connection conn = DriverManager.getConnection(url); 
+		  //Connection conn = DriverManager.getConnection(url,"mysql","root");  
 	      System.out.println("This statement after connection");
+	      }catch(SQLException ex) {
+	    	  System.out.println("state "+ex.getSQLState());
+	    	  System.out.println("message "+ex.getMessage());
+	    	  System.out.println("state "+ex.getErrorCode());
+	    	  
+	      }
 	
 	}
 
