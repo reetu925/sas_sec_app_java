3fc3e01c3a4d19e6f82ec1d8da820ed51d3dc9b0
reetu925 <mnreetu@gmail.com>
2020-05-13 14:55:11 +0530

    linux

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -17,19 +17,19 @@ public class DemoApplication {
 		 
 
 	      // Step 2: Establish the connection to the database 
-	      //String url = "jdbc:mysql://10.1.101.52:3306/sec_schema"; 
+	      String url = "jdbc:mysql://10.1.101.52:3306/sec_schema"; 
 	      //String url = "jdbc:mysql://localhost:3306/sec_schema"; 
-	      String url =  "jdbc:mysql://localhost:3306/sec_schema";
+	      //String url =  "jdbc:mysql://localhost:3306/sec_schema";
 	      System.out.println("This statement Before connection");
 	      try {
 	    	  Class.forName("com.mysql.cj.jdbc.Driver").newInstance(); 
-	      Connection conn = DriverManager.getConnection(url); 
-		  //Connection conn = DriverManager.getConnection(url,"mysql","root");  
+	    //  Connection conn = DriverManager.getConnection(url); 
+		  Connection conn = DriverManager.getConnection(url,"mysql","root");  
 	      System.out.println("This statement after connection");
 	      }catch(SQLException ex) {
 	    	  System.out.println("state "+ex.getSQLState());
 	    	  System.out.println("message "+ex.getMessage());
-	    	  System.out.println("state "+ex.getErrorCode());
+	    	  System.out.println("error "+ex.getErrorCode());
 	    	  
 	      }
 	
