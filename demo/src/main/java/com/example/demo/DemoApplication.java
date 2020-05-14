7c0a3401be6dedb506d1fce74c9e193eea680e47
reetu925 <mnreetu@gmail.com>
2020-05-14 14:28:26 +0530

    mm

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,8 +9,47 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	
+	  public static void main(String[] args) throws Exception {
+	        SpringApplication.run(DemoApplication.class, args);
+	        
+	        String SCL_URL = "jdbc:mysql://mysqlz04.sasken.com:3306/gurukulnew";
+	        String SCLuserName = "gurukulread";
+	        String SCLpassword = "hou9Ki_e";
+	        
+	        getConnection(SCL_URL, SCLuserName,SCLpassword);
+	        
+	        String SEC_URL = "jdbc:mysql://10.1.101.52:3306/sec_schema";
+	        String SECuserName = "mysql";
+	        String SECpassword = "root";
+	        
+	        getConnection(SEC_URL,SECuserName,SECpassword);
+	        
+	     }
+	        public static Connection getConnection(String url, String username, String password)
+	        {
+	            Connection connection = null;
+	            try {
+	                Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+	                System.out.println("This statement Before connection");
+	                connection = DriverManager.getConnection(url, username, password);
+	                System.out.println("connected to"+url);
 
-	public static void main(String[] args) throws Exception {
+	 
+
+	                System.out.println("connected to SEC DB");
+	            } catch (SQLException ex) {
+	                System.out.println("state " + ex.getSQLState());
+	                System.out.println("message " + ex.getMessage());
+	                System.out.println("error " + ex.getErrorCode());
+	            }
+	            catch (Exception ex) {
+	                ex.printStackTrace();
+	            }
+	            return connection;
+	    }
+
+	/*public static void main(String[] args) throws Exception {
 		SpringApplication.run(DemoApplication.class, args);
 
 		String SCL = "jdbc:mysql://mysqlz04.sasken.com:3306/gurukulnew ";
@@ -37,5 +76,5 @@ public class DemoApplication {
 		}
 
 	}
-
+*/
 }
